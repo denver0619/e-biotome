7d64e73df18fe2b77fa05c699a029632527086a4
denver0619 <janjan12345678910111213141516@gmail.com>
2022-11-19 19:31:59 +0800

    updated to work on new flutter version

---
+++ android/app/src/main/kotlin/com/example/e_biotome/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.e_biotome
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- android/app/src/main/kotlin/com/example/e_biotome_for_general_biology/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.e_biotome_for_general_biology
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
